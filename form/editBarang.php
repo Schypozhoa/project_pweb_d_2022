2f236645e569a2ab18ff18189a55292fa0749199
Danu Adi Nugroho <gaul.gaul5@gmail.com>
2022-06-16 16:40:50 +0700

    Fix path typo

--- form/editBarang.php
+++ form/editBarang.php
@@ -29,7 +29,7 @@
     <!-- navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="index.php">Inventaris Online</a>
+            <a class="navbar-brand" href="../index.php">Inventaris Online</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
